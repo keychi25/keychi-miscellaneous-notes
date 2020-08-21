ae43f434f0530d9e7faa0693ef5a7e0345435cb2
keychi25 <e165412y@st.u-gakugei.ac.jp>
2020-08-21 22:42:50 +0900

    conf : mac mini

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[This site is next sample]</p>
+        <p>[This site is next sample!!]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
