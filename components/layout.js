94c373d3d1807aa2dde8e46225727f75527615f8
keychi25 <e165412y@st.u-gakugei.ac.jp>
2021-02-03 22:50:40 +0900

    deproy確認

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Keychi Furukawa'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Keychi Blog'
 
 export default function Layout({ children, home }) {
     return (
--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,13 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[This site is next sample!!]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <h2 className={utilStyles.headingLg}>This is a my Blog</h2>
+        <Link href="mypage" as={`/mypage`}>
+          <a>mypage</a>
+        </Link>
+        <br />
+          I’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn"> Next.js tutorial</a>.
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
