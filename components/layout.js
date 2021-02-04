af81f0cc1bb56330d37e5a24d32ce4cf1836ed06
keychi25 <e165412y@st.u-gakugei.ac.jp>
2021-02-04 11:56:55 +0900

    add : img

--- components/layout.js
+++ components/layout.js
@@ -1,66 +1,72 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Keychi Furukawa'
-export const siteTitle = 'Keychi Blog'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+export const siteTitle = 'Keychiの雑記'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+  },
+}))
 
 export default function Layout({ children, home }) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="description"
-                    content="Learn how to build a personal website using Next.js"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.now.sh/${encodeURI(
-                        siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-            </Head>
-            <header className={styles.header}>
-                {home ? (
-                    <>
-                        <img
-                            src="/images/profile.jpg"
-                            className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                            alt={name}
-                        />
-                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
-                    </>
-                ) : (
-                        <>
-                            <Link href="/">
-                                <a>
-                                    <img
-                                        src="/images/profile.jpg"
-                                        className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                                        alt={name}
-                                    />
-                                </a>
-                            </Link>
-                            <h2 className={utilStyles.headingLg}>
-                                <Link href="/">
-                                    <a className={utilStyles.colorInherit}>{name}</a>
-                                </Link>
-                            </h2>
-                        </>
-                    )}
-            </header>
-            <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href="/">
-                        <a>← Back to home</a>
-                    </Link>
-                </div>
-            )}
-        </div>
-    )
+  const classes = useStyles()
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            {siteTitle}
+          </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.container}>
+            <div className={styles.backToHome}>
+              <Link href="/">
+                <a>← Back to home</a>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,80 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Date from '../components/date'
 
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Grid,
+  Container,
+} from '@material-ui/core'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 12,
+    fontSize: 25,
+  },
+})
+
 export default function Home({ allPostsData }) {
+  const classes = useStyles()
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <h2 className={utilStyles.headingLg}>This is a my Blog</h2>
-        <Link href="mypage" as={`/mypage`}>
-          <a>mypage</a>
-        </Link>
-      </section>
+      <img src="/images/header.jpg" className={utilStyles.headerImg} />
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            className={utilStyles.container}
+            zeroMinWidth
+          >
+            <Container fixed>
+              <h2 className={utilStyles.headingLg}>Posts</h2>
+              {allPostsData.map(({ id, date, title }) => (
+                <Card className={classes.root}>
+                  <CardContent>
+                    <Typography className={classes.title} color="textSecondary">
+                      {title}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      <Date dateString={date} />
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Link href="/posts/[id]" as={`/posts/${id}`}>
+                      <Button size="small">Read More</Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+              ))}
+            </Container>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            className={utilStyles.container}
+            zeroMinWidth
+          >
+            <Container fixed />
+          </Grid>
+        </Grid>
       </section>
     </Layout>
   )
@@ -44,7 +91,7 @@ export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData
-    }
+      allPostsData,
+    },
   }
 }
